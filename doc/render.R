8a7142e7fafae073ac34e85160b7f2e54baea1eb
Tanguy BARTHELEMY <tanguy.barthelemy@insee.fr>
2023-02-06 11:52:51 +0100

    adding a new render file

---
+++ doc/render.R
@@ -0,0 +1,7 @@
+
+# Rendu des fiches
+
+# Fiche 1 : Installation des outils --------------------------------------------
+
+rmarkdown::render("./doc/Fiche_1_Installation_des_outils_FR.Rmd", output_dir = "Fiches", output_format = "all")
+rmarkdown::render("./doc/Sheet_1_Tools_installation_EN.Rmd", output_dir = "Sheets", output_format = "all")
