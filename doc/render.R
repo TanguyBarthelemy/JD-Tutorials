393f37d1e4ffc1e47cfa136fb3cc4be218383de4
Tanguy BARTHELEMY <tanguy.barthelemy@insee.fr>
2023-02-28 20:00:11 +0100

    add simplified installation sheet

--- doc/render.R
+++ doc/render.R
@@ -5,3 +5,4 @@
 
 rmarkdown::render("./doc/Fiche_1_Installation_des_outils_FR.Rmd", output_dir = "Fiches", output_format = "all")
 rmarkdown::render("./doc/Sheet_1_Tools_installation_EN.Rmd", output_dir = "Sheets", output_format = "all")
+rmarkdown::render("./doc/Sheet_1_simplified_Tools_installation_EN.Rmd", output_dir = "Sheets", output_format = "all")
