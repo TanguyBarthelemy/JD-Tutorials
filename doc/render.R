7aa1e9aeb37e14f3b47047b0c8775bfdd75fce24
Tanguy BARTHELEMY <tanguy.barthelemy@insee.fr>
2023-10-04 15:14:45 +0200

    lintr project

--- doc/render.R
+++ doc/render.R
@@ -3,9 +3,9 @@
 
 # Fiche 1 : Installation des outils --------------------------------------------
 
-rmarkdown::render("./doc/Fiche_1_Installation_des_outils_FR.Rmd", 
+rmarkdown::render("./doc/Fiche_1_Installation_des_outils_FR.Rmd",
                   output_dir = "Fiches", output_format = "all")
-rmarkdown::render("./doc/Sheet_1_Tools_installation_EN.Rmd", 
+rmarkdown::render("./doc/Sheet_1_Tools_installation_EN.Rmd",
                   output_dir = "Sheets", output_format = "all")
-rmarkdown::render("./doc/Sheet_1_simplified_Tools_installation_EN.Rmd", 
+rmarkdown::render("./doc/Sheet_1_simplified_Tools_installation_EN.Rmd",
                   output_dir = "Sheets", output_format = "all")
