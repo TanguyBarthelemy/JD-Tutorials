45957861f8c8a3eb0d63f4665b512816c4328fc4
Tanguy BARTHELEMY <tanguy.barthelemy@insee.fr>
2024-03-25 18:14:16 +0100

    update render R

--- doc/render.R
+++ doc/render.R
@@ -3,9 +3,11 @@
 
 # Fiche 1 : Installation des outils --------------------------------------------
 
-rmarkdown::render("./doc/Fiche_1_Installation_des_outils_FR.Rmd",
-                  output_dir = "Fiches", output_format = "all")
-rmarkdown::render("./doc/Sheet_1_Tools_installation_EN.Rmd",
+rmarkdown::render("./doc/Fiche_1_a_Installation_des_outils_FR.Rmd",
+                  output_dir = "Fa_iches", output_format = "all")
+rmarkdown::render("./doc/Sheet_1_a_Tools_installation_EN.Rmd",
                   output_dir = "Sheets", output_format = "all")
-rmarkdown::render("./doc/Sheet_1_simplified_Tools_installation_EN.Rmd",
+rmarkdown::render("./doc/Fiche_1_b_Installation_des_outils_advanced_FR.Rmd",
+                  output_dir = "Fb_iches", output_format = "all")
+rmarkdown::render("./doc/Sheet_1_b_Tools_installation_advanced_EN.Rmd",
                   output_dir = "Sheets", output_format = "all")
