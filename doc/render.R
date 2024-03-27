1c815d407aa7000759eb39f3d86c2e1f612a2e4e
Tanguy BARTHELEMY <tanguy.barthelemy@insee.fr>
2024-03-27 10:09:42 +0100

    update AUS configuration

--- doc/render.R
+++ doc/render.R
@@ -4,10 +4,10 @@
 # Fiche 1 : Installation des outils --------------------------------------------
 
 rmarkdown::render("./doc/Fiche_1_a_Installation_des_outils_FR.Rmd",
-                  output_dir = "Fa_iches", output_format = "all")
+                  output_dir = "Fiches", output_format = "all")
 rmarkdown::render("./doc/Sheet_1_a_Tools_installation_EN.Rmd",
                   output_dir = "Sheets", output_format = "all")
 rmarkdown::render("./doc/Fiche_1_b_Installation_des_outils_advanced_FR.Rmd",
-                  output_dir = "Fb_iches", output_format = "all")
+                  output_dir = "Fiches", output_format = "all")
 rmarkdown::render("./doc/Sheet_1_b_Tools_installation_advanced_EN.Rmd",
                   output_dir = "Sheets", output_format = "all")
