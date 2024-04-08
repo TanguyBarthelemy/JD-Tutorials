950b10a258355b0749741e231b1f9a3e6a5f39bd
Tanguy BARTHELEMY <tanguy.barthelemy@insee.fr>
2024-04-08 16:58:59 +0200

    update relatives paths

--- doc/render.R
+++ doc/render.R
@@ -2,7 +2,7 @@
 # Rendu des fiches -------------------------------------------------------------
 
 # Fiches FR
-for (fiche_rmd in list.files("./doc/FR/", full.names = TRUE)) {
+for (fiche_rmd in list.files("./doc/FR/", full.names = TRUE, pattern = "*.Rmd")) {
     rmarkdown::render(
         input = fiche_rmd, 
         output_dir = "Fiches", 
@@ -11,7 +11,7 @@ for (fiche_rmd in list.files("./doc/FR/", full.names = TRUE)) {
 }
 
 # Sheets EN
-for (sheet_rmd in list.files("./doc/EN/", full.names = TRUE)) {
+for (sheet_rmd in list.files("./doc/EN/", full.names = TRUE, pattern = "*.Rmd")) {
     rmarkdown::render(
         input = sheet_rmd, 
         output_dir = "Sheets", 
