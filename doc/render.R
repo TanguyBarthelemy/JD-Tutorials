77b679ad927b9eca0ce33c25f26dfebb5d3dd80c
Tanguy BARTHELEMY <tanguy.barthelemy@insee.fr>
2024-04-08 16:21:18 +0200

    restructure tuto

--- doc/render.R
+++ doc/render.R
@@ -1,13 +1,10 @@
 
-# Rendu des fiches
+# Rendu des fiches -------------------------------------------------------------
 
-# Fiche 1 : Installation des outils --------------------------------------------
+# Fiches FR
+rmarkdown::render(list.files("./doc/FR/", full.names = TRUE),
+                  output_dir = "Fiches", output_format = "all")
 
-rmarkdown::render("./doc/Fiche_1_a_Installation_des_outils_FR.Rmd",
-                  output_dir = "Fiches", output_format = "all")
-rmarkdown::render("./doc/Sheet_1_a_Tools_installation_EN.Rmd",
-                  output_dir = "Sheets", output_format = "all")
-rmarkdown::render("./doc/Fiche_1_b_Installation_des_outils_advanced_FR.Rmd",
-                  output_dir = "Fiches", output_format = "all")
-rmarkdown::render("./doc/Sheet_1_b_Tools_installation_advanced_EN.Rmd",
+# Sheets EN
+rmarkdown::render(list.files("./doc/EN/", full.names = TRUE),
                   output_dir = "Sheets", output_format = "all")
